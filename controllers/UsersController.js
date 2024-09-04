ab48b68768c079b984bcb089eb121928bdf0a0cc
petersudai <psudai@gmail.com>
2024-09-04 01:16:15 -0700

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,33 @@
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+import dbClinet from '../utils/db';
+
+class UsersController {
+	static async postNew(req, res) {
+		const { email, password } = req.body;
+
+		if (!email) {
+			return res.status(400).json({ error: 'Missing email' });
+		}
+
+		if (!password) {
+			return res.status(400).json({ error: 'Missing password' });
+		}
+
+		const existingUser = await dbClient.db.collection('users').findOne({ email });
+		if (existingUser) {
+			return res.status(400).json({ error: 'Already exist' });
+		}
+
+		const hashedPassword = sha1(password);
+		const newUser = {
+			email,
+			password: hashedPassword,
+		};
+
+		const result = await dbClient.db.collection('users').insertOne(newUser);
+		return res.status(201).json({ id: result.insertedId, email });
+	}
+}
+
+export default UsersController;
