62d6f9e4add990ccb8c58629c77378a6b9d06d19
petersudai <psudai@gmail.com>
2024-09-04 07:06:07 -0700

    First file

---
+++ controllers/FilesController.js
@@ -0,0 +1,90 @@
+import { v4 as uuidv4 } from 'uuid';
+import { promises as fsPromises } from 'fs';
+import path from 'path';
+import mime from 'mime-types';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
+
+class FilesController {
+	static async postUpload(req, res) {
+		const token = req.headers['x-token'];
+
+		if (!token) {
+			return res.status(401).json({ error: 'Unauthorized' });
+		}
+
+		const userID = await redisClient.get(`auth_${token}`);
+		if (!userId) {
+			return res.status(401).json({ error: 'Unauthorzied' });
+		}
+
+		const { name, type, parentId = 0, isPublic = false, data } = req.body;
+
+		if (!name) {
+			return res.status(400).json({ error: 'Missing name' });
+		}
+
+		if (!['folder', 'file', 'image'].includes(type)) {
+			return res.status(400).json({ error: 'Missing type' });
+		}
+
+		if (type !== 'folder' && !data) {
+			return res.status(400).json({ error: 'Missing data' });
+		}
+
+		if (parentId !== 0) {
+			const parentFile = await dbClient.db.collection('files').findOne({ _id: new dbClient.ObjectID(parentId) });
+			if (!parentFile) {
+				return res.status(400).json({ error: 'Parent not found' });
+			}
+			if (parentFile.type !== 'folder') {
+				return res.status(400).json({ error: 'Parent is not a folder' });
+			}
+		}
+
+		const fileData = {
+			userId: new dbClient.ObjectID(userId),
+			name,
+			type,
+			isPublic,
+			parentId: parentId === 0 ? '0' : new dbClient.ObjectID(parentId),
+		};
+
+		if (type === 'folder') {
+			const result = await dbClient.db.collection('files').insertOne(fileData);
+			return res.status(201).json({
+				id: result.insertedId,
+				userId,
+				name,
+				type,
+				isPublic,
+				parentId: fileData.parentId,
+			});
+		}
+
+		const localPath = path.join(FOLDER_PATH, uuidv4());
+
+		try {
+			await fsPromises.mkdir(FOLDER_PATH, { recursive: true });
+			await fsPromises.writerFile(localPath, Buffer.from(data, 'base64'));
+
+			fileData.localPath = localPath;
+
+			const result = await dbClient.db.collection('files').insertOne(fileData);
+			return res.status(201).json({
+				id: result.insertedId,
+				userId,
+				name,
+				type,
+				isPublic,
+				parentId: fileData.parentId,
+			});
+		} catch (error) {
+			return res.status(500).json({ error: 'Unable to save the file' });
+		}
+	}
+}
+
+export default FilesController;
