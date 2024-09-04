0442b73c829640d5ca4dc055908ae12604f64273
petersudai <psudai@gmail.com>
2024-09-04 01:28:55 -0700

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,31 +3,31 @@ import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 
 class UsersController {
-	static async postNew(req, res) {
-		const { email, password } = req.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-		if (!email) {
-			return res.status(400).json({ error: 'Missing email' });
-		}
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
 
-		if (!password) {
-			return res.status(400).json({ error: 'Missing password' });
-		}
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
 
-		const existingUser = await dbClient.db.collection('users').findOne({ email });
-		if (existingUser) {
-			return res.status(400).json({ error: 'Already exist' });
-		}
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
 
-		const hashedPassword = sha1(password);
-		const newUser = {
-			email,
-			password: hashedPassword,
-		};
+    const hashedPassword = sha1(password);
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
 
-		const result = await dbClient.db.collection('users').insertOne(newUser);
-		return res.status(201).json({ id: result.insertedId, email });
-	}
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+    return res.status(201).json({ id: result.insertedId, email });
+  }
 }
 
 export default UsersController;
